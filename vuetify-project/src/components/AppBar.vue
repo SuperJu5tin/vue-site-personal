6fe2537d1de2bce2dd562ca6f1ad37328fe34bfe
SuperJu5tin <superjuslib@gmail.com>
2024-09-23 08:15:46 -0600

    finished AppBar

---
+++ vuetify-project/src/components/AppBar.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <v-app-bar :elevation="2">
+    <template #prepend>
+      <v-app-bar-nav-icon />
+    </template>
+
+    <v-app-bar-title>Justin Libhart</v-app-bar-title>
+  </v-app-bar>
+</template>
